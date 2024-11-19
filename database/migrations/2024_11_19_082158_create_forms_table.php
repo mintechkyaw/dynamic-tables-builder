9cdb00eea8c4a77634cce309914b1344c70fb305
Min Thant Kyaw <minthantk642@gmail.com>
2024-11-19 15:16:26 +0630

    database migration 0.2

--- database/migrations/2024_11_19_082158_create_forms_table.php
+++ database/migrations/2024_11_19_082158_create_forms_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->nullable();
             $table->enum('status', ['draft', 'published'])->default('draft');
             $table->timestamps();
         });
