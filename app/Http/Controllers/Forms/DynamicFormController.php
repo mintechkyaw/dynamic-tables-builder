a5a69f1b6d8addb85e6528c210ed98ae88210ae9
Khine Ko Thant <khinekothant@gmail.com>
2024-11-19 15:59:57 +0630

    simple dynamic migration

--- app/Http/Controllers/Forms/DynamicFormController.php
+++ app/Http/Controllers/Forms/DynamicFormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Forms;
 
 use App\Http\Controllers\Controller;
+use App\Models\Form;
 use Illuminate\Http\Request;
 
 class DynamicFormController extends Controller
@@ -45,5 +46,49 @@ class DynamicFormController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function generateDynamicMigration(Form $form)
+    {
+        $fields = $form->fields;
+        $migrationName = 'create_' . $form->slug . '_table';
+        $tableName = $form->slug;
+
+        $fieldDefinitions = $fields->map(function ($field) {
+            $type = match ($field->type) {
+                'text' => 'string',
+                'number' => 'integer',
+                'date' => 'date',
+                default => 'string',
+            };
+            return "\$table->{$type}('{$field->name}')->nullable();";
+        })->implode("\n            ");
+
+        $migrationContent = <<<EOT
+        <?php
+
+        use Illuminate\Database\Migrations\Migration;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Support\Facades\Schema;
+
+        return new class extends Migration {
+            public function up()
+            {
+                Schema::create('$tableName', function (Blueprint \$table) {
+                    \$table->id();
+                    \$table->timestamps();
+                    $fieldDefinitions
+                });
+            }
+
+            public function down()
+            {
+                Schema::dropIfExists('$tableName');
+            }
+        };
+        EOT;
+
+        $filePath = database_path('migrations/' . now()->format('Y_m_d_His') . "_{$migrationName}.php");
+        file_put_contents($filePath, $migrationContent);
     }
 }
