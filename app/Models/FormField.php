92d864056e15bcbe74b157342d8d7a950956bd5c
Min Thant Kyaw <minthantk642@gmail.com>
2024-11-19 15:10:33 +0630

    database migration

---
+++ app/Models/FormField.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormField extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['form_id', 'column_name', 'data_type', 'type', 'options', 'required'];
+
+    public function form() {
+        return $this->belongsTo(Form::class);
+    }
+}
