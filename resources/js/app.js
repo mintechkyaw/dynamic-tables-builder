ddccb745d41b89d5d385a5832b06a237b0dc9f80
Pyae Phyo <phyopyae.3914@gmail.com>
2024-11-20 23:50:43 +0630

    ui layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,28 @@
-import { createApp } from 'vue';
-import './bootstrap';
-import App from './App.vue';
-import router from './router';
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { createApp } from "vue";
+import "./bootstrap";
+import App from "./App.vue";
+import router from "./router";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
 const vuetify = createVuetify({
-  components,
-  directives,
-})
-
+    components,
+    directives,
+    theme: {
+        defaultTheme: "dark",
+    },
+    icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+          fa,
+        },
+      },
+});
 
 const app = createApp(App);
 app.use(router);
