d96082341d7da8d704d8cd16f783a0b36e8d550f
Pyae Phyo <phyopyae.3914@gmail.com>
2024-11-21 11:47:15 +0630

    create page

--- resources/js/pages/admin/Dashboard.page.vue
+++ resources/js/pages/admin/Dashboard.page.vue
@@ -2,8 +2,19 @@
 <v-layout>
     <v-navigation-drawer v-model="drawer">
         <v-list>
-            <v-list-item title="Navigation drawer"></v-list-item>
+            <v-list-item title="Dynamite Application"></v-list-item>
         </v-list>
+        <v-tabs direction="vertical">
+            <v-tab to="/" prepend-icon="fa-solid fa-house">
+                Home
+            </v-tab>
+            <v-tab to="/forms" prepend-icon="fa-brands fa-wpforms">
+                Forms
+            </v-tab>
+            <v-tab to="/tables" prepend-icon="fa-solid fa-database">
+                Tables
+            </v-tab>
+        </v-tabs>
     </v-navigation-drawer>
 
     <v-app-bar color="primary" prominent>
@@ -13,7 +24,7 @@
     </v-app-bar>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-        Main Content
+        <router-view></router-view>
     </v-main>
 </v-layout>
 </template>
---
+++ resources/js/pages/admin/TableSubmitPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Table Submit {{ id }}</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
