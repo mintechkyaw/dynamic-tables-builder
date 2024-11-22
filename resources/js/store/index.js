4efcf4b08d7fd1e88b432917a7936dbb498ff184
khantnaingwin <khantnaingwin532@gmail.com>
2024-11-22 10:51:42 +0630

    tables page ui published

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import Auth from "./modules/Auth";
+import Tables from "./modules/Tables";
 
 
 export default createStore({
     modules:{
         Auth,
+        Tables,
     }
 })
