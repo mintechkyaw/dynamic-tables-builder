ea705481560a1490ef672ff87c7053890b4d5c90
Pyae Phyo <phyopyae.3914@gmail.com>
2024-11-29 11:32:24 +0630

    loading field btn bug fixed

---
+++ database/migrations/2024_11_29_033904_create_marketing_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('marketing', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->notNullable();
+            $table->string('age')->notNullable();
+            $table->enum('gender', ['Male', 'Female', 'Gay'])->notNullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('marketing');
+    }
+};
