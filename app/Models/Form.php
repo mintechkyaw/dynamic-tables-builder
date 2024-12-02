b3e564628d88855182855c0de823639be3a035b8
Khine Ko Thant <khinekothant@gmail.com>
2024-12-02 11:08:50 +0630

    added uuid

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -4,12 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Form extends Model
 {
     use HasFactory;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
+    }
+
     protected $fillable = ['user_id', 'name', 'slug', 'status'];
+
+    protected $casts = [
+        'id' => 'string',
+        'user_id' => 'string',
+    ];
 
     public function user()
     {
--- database/migrations/2024_11_19_082204_create_form_fields_table.php
+++ database/migrations/2024_11_19_082204_create_form_fields_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('form_fields', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('form_id')->constrained()->cascadeOnDelete();
+            $table->uuid('id')->primary();
+            $table->uuid('form_id');
             $table->string('column_name');
             $table->enum('data_type', ['string', 'integer', 'json', 'enum', 'date']);
             $table->enum('type', ['text', 'number', 'check_box', 'radio', 'calendar']);
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->boolean('required')->default(false);
             $table->timestamps();
             $table->unique(['form_id', 'column_name']);
+
+            $table->foreign('form_id')->references('id')->on('forms')->cascadeOnDelete();
         });
     }
 
