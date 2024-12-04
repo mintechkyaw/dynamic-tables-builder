de787b0b59c3ee7c4c4b5a4f4cb84acbf21b3f7b
Pyae Phyo <phyopyae.3914@gmail.com>
2024-12-04 16:15:27 +0630

    ui setting

---
+++ resources/js/pages/admin/RolePage.vue
@@ -0,0 +1,48 @@
+<template>
+<v-container>
+    <v-row>
+        <v-col cols="6">
+            <h2 class="mb-2">Create Role</h2>
+            <v-text-field label="Enter Role"></v-text-field>
+            <v-btn variant="tonal">Create</v-btn>
+        </v-col>
+        <v-col>
+            <h2>Table</h2>
+            <v-table>
+                <thead>
+                    <tr>
+                        <th class="text-left">
+                            Id
+                        </th>
+                        <th class="text-left">
+                            Role Name
+                        </th>
+                        <th class="text-left">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>admin</td>
+                        <td>
+                            <v-btn color="warning" size="x-small">Delete</v-btn>
+                        </td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 import Auth from "./modules/Auth";
 import Forms from "./modules/Forms";
+import Users from "./modules/Users";
 
 
 export default createStore({
     modules:{
         Auth,
         Forms,
+        Users
     }
 })
