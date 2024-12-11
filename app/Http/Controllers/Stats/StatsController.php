569d97739999459a2ea257e4b3d9928bf0359b51
Min Thant Kyaw <minthantk642@gmail.com>
2024-12-11 15:44:30 +0630

    user update email validation ignoring current user id and permissions sync instead of attach to avoid duplicate

--- app/Http/Controllers/Stats/StatsController.php
+++ app/Http/Controllers/Stats/StatsController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Stats;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
 
 class StatsController extends Controller
 {
     public function index()
     {
-        return 'asdf';
+        // $userCount;
+        // roles
+        // permisions
+        // tables
+        // forms
+
+        $usersCount = User::count();
+        $roleCount = Role::count();
+        $permissionsCount = Permission::count();
+
     }
 }
