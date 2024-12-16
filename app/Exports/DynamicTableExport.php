bfb9ec3d43f2fe9f49f0989a11f5b42d35d9dc90
Khine Ko Thant <khinekothant@gmail.com>
2024-12-16 14:55:34 +0630

    Updated output excel table style

--- app/Exports/DynamicTableExport.php
+++ app/Exports/DynamicTableExport.php
@@ -5,8 +5,10 @@ namespace App\Exports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class DynamicTableExport implements FromCollection, WithHeadings
+class DynamicTableExport implements FromCollection, WithHeadings, WithStyles
 {
     protected $data;
 
@@ -16,6 +18,29 @@ class DynamicTableExport implements FromCollection, WithHeadings
     {
         $this->data = $data;
         $this->headers = $headers;
+    }
+    public function styles(Worksheet $sheet)
+    {
+        $highestColumn = $sheet->getHighestColumn();
+        $highestRow = $sheet->getHighestRow();
+        $dataRange = "A1:{$highestColumn}{$highestRow}";
+        return [
+            1 => [
+                'font' => ['bold' => true],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['argb' => 'FF0CF52B'], // Light blue color
+                ],
+            ],
+            $dataRange => [
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => 'FF000000'],
+                    ],
+                ],
+            ],
+        ];
     }
 
     public function collection()
